e03b4d09bfadce27cf9c78cafde3aedb69e53b87
HenryChang223 <henrychang223@gmail.com>
2024-02-07 11:51:16 +0800

    b
    b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "368891ae71d92c2775204a319d28cc09
 "assets/AssetManifest.bin.json": "7616615e8094270b57efdc26cdf35464",
 "assets/AssetManifest.json": "51be3db0d8d302ddaf828e5af01e47ed",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "037502c18f59de92042b3c4ab4ab95fc",
+"assets/fonts/MaterialIcons-Regular.otf": "30ec91a1d30f1968e00bd5566dc0a58c",
 "assets/images/add_song.png": "b91a26be7bc03f5bc9f25a34d5577067",
 "assets/images/del_song.png": "70113956aecfe0f4ec9ff071521bc4dc",
 "assets/images/machine.ico": "62f07c49051772eca2713b5172c56bae",
@@ -33,9 +33,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "368891ae71d92c2775204a319d28cc09
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8a300528390618da19b5470dc4563de0",
-"/": "8a300528390618da19b5470dc4563de0",
-"main.dart.js": "1f61f085e54cd65c23169e74b8ce73b2",
+"index.html": "a8b21d707abb3aa11fa3282d678f1c3d",
+"/": "a8b21d707abb3aa11fa3282d678f1c3d",
+"main.dart.js": "95a0856db689a40a9e17cf95c3dc7fa3",
 "manifest.json": "04e8d3181f41704f39ed386cc10521ba",
 "version.json": "571f7f349faccfa35775343c0ba1859f"};
 // The application shell files that are downloaded before a service worker can
